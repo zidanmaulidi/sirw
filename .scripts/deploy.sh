2eab1b66f60cd24245ced925f74ff33e5d45e045
yoe174 <yoechannelpjr@gmail.com>
2024-06-14 00:47:09 +0700

    setting

---
+++ .scripts/deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+echo "Deployment started ..."
+
+# Enter maintenance mode or return true
+# if already is in maintenance mode
+(php artisan down) || true
+
+# Stash
+git stash
+# Pull the latest version of the app
+git pull origin master
+
+# Install composer dependencies
+composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+
+#storage link
+php artisan storage:link
+
+# Clear the old cache
+php artisan clear-compiled
+
+# Recreate cache
+php artisan optimize
+
+# Exit maintenance mode
+php artisan up
+
+echo "Deployment finished!"
